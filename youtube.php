3215d3601ac152f2a7c28a1a95cfd47b8c5ea7d8
Lucas Mouilleron <lucas.mouilleron@me.com>
2014-09-24 15:06:33 +0200

    metas

--- youtube.php
+++ youtube.php
@@ -7,7 +7,7 @@
     <p>todo ...</p>
 </div>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Aenean lacinia bibendum nulla sed consectetur.</p>
+<p>todo</p>
 
 <!-- /////////////////////////////////////////////////////////////// -->
 <?php include "includes/footer.php" ?>
