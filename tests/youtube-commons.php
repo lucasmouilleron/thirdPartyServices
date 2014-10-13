7c108081ddf58ecd66f5265b5b24ca75eb4f6442
Lucas Mouilleron <lmouille@frliopww0064.cachemire.pub>
2014-10-13 13:35:20 +0200

    yt server pulling

--- tests/youtube-commons.php
+++ tests/youtube-commons.php
@@ -3,8 +3,13 @@
 /////////////////////////////////////////////////////////////
 define("VIDEO_TEST_ID","0f12GBhib1k");
 define("VIDEO_TEST2_ID","P_i1HMOcPQE");
+define("YOUTUBE_API_KEY", "AIzaSyBDXHIwGk5qX5dK4PvfAXPQOrXpoVUQiJQ");
+define("YOUTUBE_PLAYLIST_ID","PL63F0C78739B09958");
+define("CACHE_PATH",__DIR__."/../cache/");
 
 /////////////////////////////////////////////////////////////
 require_once  __DIR__."/../libs/tools.php";
+require __DIR__."/../libs/SimpleCache.php";
+require_once __DIR__."/../libs/Google/autoload.php";
 
 ?>
